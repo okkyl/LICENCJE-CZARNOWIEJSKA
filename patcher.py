894c5dbf6412083f1e6bb56c25b6a9a8ba46fcf6
okkyl_ <94751425+okkyl@users.noreply.github.com>
2021-12-01 12:42:10 +0100

    Update patcher.py

--- patcher.py
+++ patcher.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = 'https://raw.githubusercontent.com/okkyl/HARDWARE-INFO/main/main.py'
+url = 'https://raw.githubusercontent.com/okkyl/LICENCJE-CZARNOWIEJSKA/main/LICENCJE.BAT'
 r = requests.get(url, allow_redirects=True)
 
-open('main.py', 'wb').write(r.content)
+open('LICENCJE.BAT', 'wb').write(r.content)
